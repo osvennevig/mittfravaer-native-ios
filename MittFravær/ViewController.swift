41e4370a0b1b3e47328df89875405f92e31e8c82
Ole Christian Hondrelis <web@powernytt.no>
2018-11-29 12:22:47 +0100

    Initial Commit

---
+++ MittFravær/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MittFravær
+//
+//  Created by Ole Svennevig on 29/11/2018.
+//  Copyright © 2018 kvitér. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
